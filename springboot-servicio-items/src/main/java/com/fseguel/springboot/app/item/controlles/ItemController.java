64eb3e7a2700356095d699660ba8af80d6d623a7
f.segueljeria <f.segueljeria@gmail.com>
2022-03-23 20:22:27 -0300

    Configurando servidor de configuraciones
    configs

--- springboot-servicio-items/src/main/java/com/fseguel/springboot/app/item/controlles/ItemController.java
+++ springboot-servicio-items/src/main/java/com/fseguel/springboot/app/item/controlles/ItemController.java
@@ -1,13 +1,20 @@
 package com.fseguel.springboot.app.item.controlles;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,10 +29,14 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
-	private final Logger log = LoggerFactory.getLogger(ItemController.class);
+	private Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
@@ -33,6 +44,9 @@ public class ItemController {
 	@Autowired
 	@Qualifier("itemServiceFeignImpl")
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(@RequestParam(name="nombre", required = false) String nombre, @RequestHeader(name="token-request", required = false) String token){
@@ -61,6 +75,23 @@ public class ItemController {
 	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return CompletableFuture.supplyAsync( () -> itemService.findById(id, cantidad));
 	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		log.info(texto);
+		Map<String, String> json = new HashMap<>(); 
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
+	}
+	
+	
 	
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
 		log.info(e.getMessage());
