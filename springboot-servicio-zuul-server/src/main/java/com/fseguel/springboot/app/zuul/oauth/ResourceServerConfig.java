2d6f14ab9044bc0548a4e2068b2d37e6826a62ec
f.segueljeria <f.segueljeria@gmail.com>
2022-04-13 11:12:16 -0400

    Autenticación Oauth2

--- springboot-servicio-zuul-server/src/main/java/com/fseguel/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ springboot-servicio-zuul-server/src/main/java/com/fseguel/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.fseguel.springboot.app.zuul.oauth;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -91,7 +92,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter jwtConverter = new JwtAccessTokenConverter();
-		jwtConverter.setSigningKey(jwtKey);
+		jwtConverter.setSigningKey(Base64.getEncoder().encodeToString(jwtKey.getBytes()));
 		return jwtConverter;
 	}
 	
